8c7ba6a3010eb68113f541bdcc59af3713d5b716
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2025-08-07 16:27:38 -0300

    Init android project

---
+++ app/src/main/java/com/fernando/gerenciadordeloja/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.fernando.gerenciadordeloja.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fernando.gerenciadordeloja.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
